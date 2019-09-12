f15c7a1f066167aea593cce8c695430c78ac2c4b
Suleman Kadiri <Suleman.Kadiri@students.ittralee.ie>
2019-09-12 12:14:09 +0100

    Initial commit

---
+++ src/skspackage/FirstCalss.java
@@ -0,0 +1,10 @@
+package skspackage;
+
+public class FirstCalss {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+    }
+}
---
+++ src/skspackage/SecondClass.java
@@ -0,0 +1,13 @@
+package skspackage;
+
+import javax.swing.*;
+
+public class SecondClass {
+    public static void main(String[] args) {
+
+
+        String name = JOptionPane.showInputDialog("Please Enter Your name");
+    }
+
+
+}
