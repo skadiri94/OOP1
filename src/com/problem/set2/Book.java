46a8b4516a6e716c18949bba55d13264fea3a459
Skadiri94 <skadiri94@yahoo.com>
2019-09-24 16:53:10 +0100

    update 24/09/2019

--- src/com/problem/set2/Book.java
+++ src/com/problem/set2/Book.java
@@ -14,7 +14,7 @@ public class Book{
      //the no argument constructions method begins here.
      public  Book(){
 
-      this.title = "";
+      this.title = "No Title";
       this.price = 0;
       this.ISBN =  "";
       this.numberOfPages = 0;
@@ -24,10 +24,10 @@ public class Book{
     //the multiple argument constructor
      public  Book(String title, float price, String isbn, int numberOfPages){
 
-         this.title = getTitle();
-         this.price = getPrice();
-         this.ISBN = getISBN();
-         this.numberOfPages = getnumberOfPages();
+         setTitle(title);
+         setPrice(price);
+         setISBN(isbn);
+         setNumberOfPages(numberOfPages);
 
 
 
@@ -36,49 +36,61 @@ public class Book{
     //the beginning of accessor methods.
     public String getTitle(){
 
-         return JOptionPane.showInputDialog(null, "Please Enter the Title: \n" , "Book Title",JOptionPane.INFORMATION_MESSAGE);
+         //return JOptionPane.showInputDialog(null, "Please Enter the Title: \n" , "Book Title",JOptionPane.INFORMATION_MESSAGE);
+        return title;
     }
 
     public float getPrice(){
 
-        return Float.parseFloat(JOptionPane.showInputDialog(null, "Please Enter Price: \n" , "Book Price",JOptionPane.INFORMATION_MESSAGE));
+        //return Float.parseFloat(JOptionPane.showInputDialog(null, "Please Enter Price: \n" , "Book Price",JOptionPane.INFORMATION_MESSAGE));
+
+        return price;
     }
 
     public String getISBN(){
 
-        return JOptionPane.showInputDialog(null, "Please Enter ISBN: \n" , "Book ISBN",JOptionPane.INFORMATION_MESSAGE);
+        //return JOptionPane.showInputDialog(null, "Please Enter ISBN: \n" , "Book ISBN",JOptionPane.INFORMATION_MESSAGE);
+
+        return ISBN;
     }
 
     public int getnumberOfPages(){
 
-        return Integer.parseInt(JOptionPane.showInputDialog(null, "Please Enter the Number Of Pages: \n" , "Pages Of Book",JOptionPane.INFORMATION_MESSAGE));
+        //return Integer.parseInt(JOptionPane.showInputDialog(null, "Please Enter the Number Of Pages: \n" , "Pages Of Book",JOptionPane.INFORMATION_MESSAGE));
+
+        return numberOfPages;
     }
     //End of Accessor Method.
 
     //Beginning of Mutator method.
     public void setTitle(String title){
 
-        this.title = getTitle();
+         if(title.equals("")) {
+             JOptionPane.showMessageDialog(null, "....", "Error", JOptionPane.ERROR_MESSAGE);
+             this.title = "No title";
+         }
+         else
+             this.title = title;
     }
 
-    public void setPrice(){
+    public void setPrice(float price){
 
-        this.price = getPrice();
+        this.price = price;
     }
 
     public void setISBN(String isbn){
 
-        this.ISBN = getISBN();
+        this.ISBN = isbn;
     }
 
-    public void numberOfPages(int nOP){
+    public void setNumberOfPages(int nOP){
 
-        this.numberOfPages = getnumberOfPages();
+        this.numberOfPages =nOP;
     }//End of Mutator Method.
 
     public String toString(){
 
-         return   "Title: " + getTitle() + "\nPrice: " + getPrice() + "\nISBN: "  + getISBN() + "\nNumber of pages: " + getnumberOfPages();
+         return   "Title: " + title + "\nPrice: " + price + "\nISBN: "  + ISBN + "\nNumber of pages: " + numberOfPages;
     }//End of toString Method which display the content of the object into string.
 
 
--- src/com/problem/set2/BookSystem.java
+++ src/com/problem/set2/BookSystem.java
@@ -6,12 +6,30 @@ public class BookSystem {
 
     public static void main(String []args){
 
-        Book sk;
+        Book sk, sk2, sk3;
 
         sk = new Book();
+        sk3 = new Book("", 120,"agheo46042654",675);
 
+        JOptionPane.showMessageDialog(null, sk.toString(),"My new Object", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, sk3.toString(),"My new Object", JOptionPane.INFORMATION_MESSAGE);
 
-        JOptionPane.showMessageDialog(null, sk.toString() ,"My new Object", JOptionPane.INFORMATION_MESSAGE);
+        String title = JOptionPane.showInputDialog(null, "Please Enter the Title: \n" , "Book Title",JOptionPane.INFORMATION_MESSAGE);
+
+        while(title.equals(""))
+        {
+
+        }
+
+        float price = Float.parseFloat(JOptionPane.showInputDialog(null, "Please Enter Price: \n" , "Book Price",JOptionPane.INFORMATION_MESSAGE));
+
+        String isbn = JOptionPane.showInputDialog(null, "Please Enter ISBN: \n" , "Book ISBN",JOptionPane.INFORMATION_MESSAGE);
+
+        int numOfPages = Integer.parseInt(JOptionPane.showInputDialog(null, "Please Enter the Number Of Pages: \n" , "Pages Of Book",JOptionPane.INFORMATION_MESSAGE));
+
+        sk2 = new Book(title,price,isbn,numOfPages);
+
+        JOptionPane.showMessageDialog(null, sk.toString() + "\n\n" + sk2.toString() +"\n\n" + sk3.toString(),"My new Object", JOptionPane.INFORMATION_MESSAGE);
 
 
     }
