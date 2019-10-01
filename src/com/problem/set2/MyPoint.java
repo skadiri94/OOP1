e05ab4c010863a4e18cfdc6c8c37e17e1bd87549
OOP1 <skadiri94@yahoo.com>
2019-10-01 14:02:29 +0100

    update 01/01/2019

---
+++ src/com/problem/set2/MyPoint.java
@@ -0,0 +1,49 @@
+package com.problem.set2;
+
+public class MyPoint {
+
+    private float xVal;
+    private float yVal;
+
+    public MyPoint(){
+
+        this(0,0);
+    }
+    public MyPoint(float xVal, float yVal){
+
+        setXVal(xVal);
+        setYVal(yVal);
+    }
+
+    public void setXVal(float xVal){this.xVal = xVal;}
+    public void setYVal(float yVal){this.yVal = yVal;}
+
+    public float getxVal(){return xVal;}
+    public float getyVal(){return yVal;}
+
+    public String toString(){
+
+        return "Point";
+    }
+
+    public float moveHorizontally(){
+
+
+
+    }
+
+    public float moveVertically(){
+
+
+    }
+
+    public float distanceFromOrigin(){
+
+
+    }
+
+    public float translate(){
+
+    }
+
+}
