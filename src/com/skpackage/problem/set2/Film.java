777b701cc01903cb193eb5a11bce0e766b35bb12
skadiri94 <skadiri94@yahoo.com>
2019-10-09 01:48:40 +0100

    Update 09/10/2019

--- src/com/skpackage/problem/set2/Film.java
+++ src/com/skpackage/problem/set2/Film.java
@@ -1,4 +1,57 @@
 package com.skpackage.problem.set2;
 
 public class Film {
+
+    private String title;
+    private String director;
+    private int duration;
+    private static int filmCount = 0;
+
+    public Film(){
+        this("Unknown Title","Unknown Director",0);
+    }
+
+
+
+    public Film(String title, String director, int duration){
+
+        setTitle(title);
+        setDirector(director);
+        setDuration(duration);
+        filmCount++;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public static int getFilmCount() {
+        return filmCount;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String toString() {
+        return "Film: " + title + "\n" +
+                "Director: " + director + "\n" +
+                "Duratio: " + duration ;
+    }
 }
--- src/com/skpackage/problem/set2/MyFlicks.java
+++ src/com/skpackage/problem/set2/MyFlicks.java
@@ -1,4 +1,55 @@
 package com.skpackage.problem.set2;
 
+import javax.swing.*;
+
 public class MyFlicks {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        Film[] catalog = new Film[10];
+        String title, director;
+        int duration;
+
+
+
+        while (JOptionPane.showConfirmDialog(null, "Would you Like to Enter a Movie Detail ?",
+                "Catalog", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+            title = JOptionPane.showInputDialog(null, "Enter Movie Title:");
+
+            director = JOptionPane.showInputDialog(null, "Enter Director Name: ");
+
+            duration = Integer.parseInt(JOptionPane.showInputDialog("Enter Duration Of Movie: "));
+
+            catalog[i] = new Film(title, director, duration);
+
+            i++;
+
+        }
+
+        for (int j = 0; j < catalog.length; j++) System.out.println("\n" + catalog[j].toString());
+
+        displayFilms(catalog);
+
+
+    }
+
+    public static void displayFilms(Film[] a){
+
+        //int filmCount = Film.getFilmCount();
+
+        JTextArea jta = new JTextArea();
+        jta.setText("Number of Films: " + "\n\n");
+
+        for(int i = 0; i < a.length; i ++){
+
+            jta.append(a[i].toString() + "\n\n");
+
+        }
+    System.out.println(jta);
+
+        JOptionPane.showMessageDialog(null,jta,"Catalog",JOptionPane.INFORMATION_MESSAGE);
+
+    }
 }
